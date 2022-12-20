4a34c099f49934c3748febd5c3bf9b36e40ad8dd
Mohamed Khaled Elzalook <114666874+Morales020@users.noreply.github.com>
2022-12-20 16:27:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter The Start Number : ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter The End Number : ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Prime Numbers Between {0} And {1} is : ",num1,num2);
+
+            for (int i = num1;i<= num2;i++)
+            {
+                int counter = 0;
+                for (int j = 2; j <= i/2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+            if (counter==0 && i >1)
+                    {
+                       Console.WriteLine( i );
+                    }
+            }
+        }
+    }
+}
